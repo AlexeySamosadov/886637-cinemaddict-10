5c3c5ea95797e31d0ee0f93232e866320fcdc9da
AlexeySamosadov <asamosadov@gmail.com>
2020-01-10 04:12:28 +0300

    ИСправил все ошибки - стали принимать не обьект а массив

--- src/components/sort-navigation.js
+++ src/components/sort-navigation.js
@@ -1,16 +1,17 @@
 import AbstractComponent from "./abstract-component";
 
 export const SortType = {
+  DEFAULT: `default`,
   DATE: `date`,
   RATING: `rating`,
-  DEFAULT: `default`
 };
 
-export const getSortNavigationTemplate = () => {
+export const getSortNavigationTemplate = (typeOfSorting) => {
+  typeOfSorting = Object.values(typeOfSorting);
   return (`<ul class="sort">
-    <li><a href="#" data-sort-type ="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" data-sort-type ="${SortType.DATE}" class="sort__button">Sort by date</a></li>
-    <li><a href="#" data-sort-type ="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" data-sort-type ="${typeOfSorting[0]}" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" data-sort-type ="${typeOfSorting[1]}" class="sort__button">Sort by date</a></li>
+    <li><a href="#" data-sort-type ="${typeOfSorting[2]}" class="sort__button">Sort by rating</a></li>
   </ul>`);
 };
 
@@ -21,7 +22,7 @@ export default class SortNavigation extends AbstractComponent {
     this._currentSortType = SortType.DEFAULT;
   }
   getTemplate() {
-    return getSortNavigationTemplate();
+    return getSortNavigationTemplate(SortType);
   }
 
   setSortTypeHandler(handler) {
